de8cfd9a1348d950abe15e1c3f74a4adc53ae77d
jkarthick89 <jkarthick89@gmail.com>
2016-02-19 02:00:35 +0530

    First

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,70 @@
+﻿using Sytem;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBProviderFactory oDBFactory = new SQLClient();
+            
+        }
+    }
+
+    class DBCommand
+    {
+    }
+    class DBConnection
+    {
+    }
+    abstract class DBProviderFactory
+    {
+        DBCommand CreateCommand();
+        DBConnection CreateConnection();
+    }
+
+    class SQLClient : DBProviderFactory
+    {
+        DB
+    }
+
+    class OracleClient : DBProviderFactory
+    {
+    }
+
+    class OLEDBClient : DBProviderFactory
+    {
+    }
+
+
+
+    class SQLConnection : DBConnection
+    {
+    }
+
+    class OLEDBConnection : DBConnection
+    {
+    }
+
+    class OracleConnection : DBConnection
+    {
+    }
+
+
+
+    class SQLCommand : DBCommand
+    {
+    }
+
+    class OLEDBCommand : DBCommand
+    {
+    }
+
+    class OracleCommand : DBCommand
+    {
+    }
+}
